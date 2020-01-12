83a4a070445a78b3771158cf60a28be83cefc22a
kyChu <hello.kychu@gmail.com>
2020-01-12 21:05:47 +0800

    optimize this module

--- NavBoard/Calibration/ViewerChart/ViewerChart.java
+++ NavBoard/Calibration/ViewerChart/ViewerChart.java
@@ -3,6 +3,7 @@ package Calibration.ViewerChart;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -14,14 +15,18 @@ import Calibration.ViewerChart.Component.Line2D;
 public class ViewerChart extends JPanel implements Runnable {
 	private static final long serialVersionUID = 1L;
 
+	private Semaphore DotListMutex = null;
 	private ArrayList<Dot2D> DotList = null;
+	private Semaphore LineListMutex = null;
 	private ArrayList<Line2D> LineList = null;
 
 	private Coord2D coord = null;
 	public ViewerChart(String hName, String vName, Color hColor, Color vColor) {
 		coord = new Coord2D(hName, vName, hColor, vColor);
 
+		DotListMutex = new Semaphore(1);
 		DotList = new ArrayList<Dot2D>();
+		LineListMutex = new Semaphore(1);
 		LineList = new ArrayList<Line2D>();
 
 //		DotList.add(new Dot2D(1, 1, Color.WHITE));
@@ -38,20 +43,68 @@ public class ViewerChart extends JPanel implements Runnable {
 	}
 
 	public void addDot2D(Dot2D d) {
-		DotList.add(d);
-		if(this.isShowing() == true) {
-			Graphics g = this.getGraphics();
-			coord.transform(d);
-			d.show(g);
+		try {
+			DotListMutex.acquire();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		DotList.add(d);
+		DotListMutex.release();
+//		if(this.isShowing() == true) {
+//			Graphics g = this.getGraphics();
+//			coord.transform(d);
+//			d.show(g);
+//		}
+	}
+
+	public void removeAllDot2D() {
+		try {
+			DotListMutex.acquire();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		DotList.clear();
+		DotListMutex.release();
+//		if(this.isShowing() == true) {
+//			this.repaint();
+//		}
 	}
 
 	public void addLine2D(Line2D l) {
+		try {
+			LineListMutex.acquire();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		LineList.add(l);
+		LineListMutex.release();
+//		if(this.isShowing() == true) {
+//			Graphics g = this.getGraphics();
+//			coord.transform(l);
+//			l.show(g);
+//		}
+	}
+
+	public void removeAllLine2D() {
+		try {
+			LineListMutex.acquire();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		LineList.clear();
+		LineListMutex.release();
+//		if(this.isShowing() == true) {
+//			this.repaint();
+//		}
+	}
+
+	public void refresh() {
 		if(this.isShowing() == true) {
-			Graphics g = this.getGraphics();
-			coord.transform(l);
-			l.show(g);
+			this.repaint();
 		}
 	}
 
@@ -60,14 +113,28 @@ public class ViewerChart extends JPanel implements Runnable {
 		g.fillRect(0, 0, this.getWidth(), this.getHeight());
 		coord.update(this.getWidth(), this.getHeight());
 		coord.show(g);
+		try {
+			DotListMutex.acquire();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		for(Dot2D d : DotList) {
 			coord.transform(d);
 			d.show(g);
+		}
+		DotListMutex.release();
+		try {
+			LineListMutex.acquire();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		for(Line2D l : LineList) {
 			coord.transform(l);
 			l.show(g);
 		}
+		LineListMutex.release();
 	}
 
 	@Override
@@ -93,6 +160,20 @@ public class ViewerChart extends JPanel implements Runnable {
 			addDot2D(new Dot2D(x, y, Color.WHITE));
 			addLine2D(new Line2D(0, 0, x, y, Color.GREEN));
 		} while((-- i) != 0);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		removeAllLine2D();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		removeAllDot2D();
 	}
 
 	public static void main(String[] args) {
