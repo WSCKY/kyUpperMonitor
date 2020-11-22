c5f57d76bfd41a414f4e6c56060a1119b635230a
kyChu <hello.kychu@gmail.com>
2020-11-22 15:42:49 +0800

    add error string to indicate error information

--- kyMainFrame/Linux/src/CommTool/exception/ReadDataFailure.java
+++ kyMainFrame/Linux/src/CommTool/exception/ReadDataFailure.java
@@ -6,10 +6,17 @@ public class ReadDataFailure extends Exception {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	private String err_str = null;
 	public ReadDataFailure() {}
+	public ReadDataFailure(String err) {
+		this.err_str = err;
+	}
 
 	@Override
 	public String toString() {
+		if(this.err_str != null) {
+			return "error while reading data! (" + this.err_str + ")";
+		}
 		return "error while reading data!";
 	}
 	
