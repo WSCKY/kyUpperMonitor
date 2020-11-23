fbdb3d7b6d7900e863b6063769f58bd81aeea815
kyChu <hello.kychu@gmail.com>
2020-11-24 01:01:16 +0800

    fix table renferer (high cpu usage)

--- kyLinkWaveTool/src/kyLinkWaveTool/TableColorRenderer/ColorTableRenderer.java
+++ kyLinkWaveTool/src/kyLinkWaveTool/TableColorRenderer/ColorTableRenderer.java
@@ -10,49 +10,53 @@ import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
 
-public class ColorTableRenderer {
-	private Color defaultBackground = null;
-	private Color defaultSelectionBackground = null;
+public class ColorTableRenderer extends DefaultTableCellRenderer {
+	private static final long serialVersionUID = 1993L;
 	private ArrayList<TableRowColor> tabCol = null;
 
-	DefaultTableCellRenderer TableColorRenderer = new DefaultTableCellRenderer() {
-		private static final long serialVersionUID = 1L;
-
-		public Component getTableCellRendererComponent(JTable table,Object value, boolean isSelected, boolean hasFocus,int row, int column) {
-			this.setBackground(defaultBackground);
-			this.setHorizontalAlignment(SwingConstants.CENTER);
-			if(column != 0) {
-				for(TableRowColor tc : tabCol) {
-					if(tc.row == row) {
-						setBackground(tc.color);
-						if(isSelected) {
-							table.setSelectionBackground(tc.color);
-						}
-						break;
-					}
+	@Override
+	public Component getTableCellRendererComponent(JTable table,Object value, boolean isSelected, boolean hasFocus,
+      int row, int column) {
+		// TODO Auto-generated method stub
+		setHorizontalAlignment(SwingConstants.CENTER);
+		boolean bg_set = false;
+		if(column != 0) {
+			for(TableRowColor tc : tabCol) {
+				if(tc.row == row) {
+					setBackground(tc.color);
+					bg_set = true;
+					break;
 				}
 			}
-			Component comp = super.getTableCellRendererComponent(table, value,isSelected, hasFocus, row, column);
-			table.setSelectionBackground(defaultSelectionBackground);
-			return comp;
 		}
-	};
+		if(!bg_set) this.setBackground(Color.WHITE);
+		// extends super default renderer
+		return super.getTableCellRendererComponent(table, value,isSelected, hasFocus, row, column);
+	}
 
 	public ColorTableRenderer() {
 		tabCol = new ArrayList<TableRowColor>();
-		defaultBackground = Color.WHITE;
-		defaultSelectionBackground = Color.LIGHT_GRAY;
 	}
 
 	public void applyTableColorRenderer(TableColumnModel tcm) {
         for (int i = 1, n = tcm.getColumnCount(); i < n; i++) {
             TableColumn tc = tcm.getColumn(i);
-            tc.setCellRenderer(TableColorRenderer);
+            tc.setCellRenderer(this);//(TableColorRenderer);
         }
 	}
 
 	public void clearRowColor() {
 		tabCol.clear();
+	}
+
+	public Color getRowColor(int row)
+	{
+		for(TableRowColor tc : tabCol) {
+			if(tc.row == row) {
+				return tc.color;
+			}
+		}
+		return null;
 	}
 
 	public void addRowColor(int row, Color c) {
