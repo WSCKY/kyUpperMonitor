32f8fbcf7a6071f23fbf9c2f9ed23023c41bcc76
kyChu <hello.kychu@gmail.com>
2020-11-25 10:49:13 +0800

    add Reentrant Lock for list

--- kyLinkWaveTool/src/kyLinkWaveTool/Observer/Observer.java
+++ kyLinkWaveTool/src/kyLinkWaveTool/Observer/Observer.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.concurrent.locks.ReentrantLock;
 
 import kyLinkWaveTool.ColorAlloc.ColorAlloc;
 
 public class Observer {
 	private ColorAlloc allocColor = new ColorAlloc();
+	private ReentrantLock listLock = new ReentrantLock();
 	private ArrayList<ObserveMbr> ObsMbrList = new ArrayList<ObserveMbr>();
 	private Collection<ObserverEventListener> ObsEvtListener = new HashSet<ObserverEventListener>();
 
@@ -31,37 +33,48 @@ public class Observer {
 	public Color addObserveMbr(ObserveMbr om) {
 		Color c = allocColor.getColor();
 		om.setColor(c);
+		listLock.lock();
 		ObsMbrList.add(om);
+		listLock.unlock();
 		publishAddEvent(new ObserverEvent(om));
 		return c;
 	}
 
 	public void removeObserveMbr(ObserveMbr om) {
+		listLock.lock();
 		for(ObserveMbr mbr : ObsMbrList) {
 			if(mbr.getGroupId() == om.getGroupId() && mbr.getMbrOffset() == om.getMbrOffset()) {
 				allocColor.releaseColor(mbr.getColor());
 				ObsMbrList.remove(mbr);
 				publishRemoveEvent(new ObserverEvent(mbr));
+				listLock.unlock();
 				return; /* search end */
 			}
 		}
+		listLock.unlock();
 	}
 
 	public void removeObserveMbr(String MbrName) {
+		listLock.lock();
 		for(ObserveMbr mbr : ObsMbrList) {
 			if(mbr.getMbrName().equals(MbrName)) {
 				allocColor.releaseColor(mbr.getColor());
 				ObsMbrList.remove(mbr);
 				publishRemoveEvent(new ObserverEvent(mbr));
+				listLock.unlock();
 				return; /* search end */
 			}
 		}
+		listLock.unlock();
 	}
+
+	public void takObserveMbr() {listLock.lock();}
+	public void relObserveMbr() {listLock.unlock();}
 
 	public void addObserverEventListener(ObserverEventListener l) {
 		ObsEvtListener.add(l);
 	}
-	
+
 	public void removeObserverEventListener(ObserverEventListener l) {
 		ObsEvtListener.remove(l);
 	}
@@ -71,6 +84,9 @@ public class Observer {
 	}
 
 	public int getObserveNumber() {
-		return ObsMbrList.size();
+		listLock.lock();
+		int s = ObsMbrList.size();
+		listLock.unlock();
+		return s;
 	}
 }
